537333b073fd4267e16636b73e7870a8369aee39
Peter Hellberg <peter@c7.se>
2013-05-12 03:20:54 +0200

    New specs for name, number, nutrient_values and get_data!

--- spec/mat/foodstuff_spec.rb
+++ spec/mat/foodstuff_spec.rb
@@ -14,4 +14,40 @@ describe Mat::Foodstuff do
       subject.new("foo").data.must_equal "foo"
     end
   end
+
+  describe "name" do
+    it "returns the name from the data" do
+      foodstuff.name.must_equal "Dressing majonnäs fett ca 40%"
+    end
+  end
+
+  describe "number" do
+    it "returns the number from the data" do
+      foodstuff.number.must_equal 44
+    end
+  end
+
+  describe "nutrient_values" do
+    it "returns the nutrient values for the food" do
+      foodstuff.nutrient_values['energyKj'].must_equal 1898
+    end
+
+    it "it retrieves more data if needed" do
+      food = subject.new("number" => 44)
+
+      Mat.api.stub(:foodstuff, fixture_data) do
+        food.nutrient_values['energyKj'].must_equal 1898
+      end
+    end
+  end
+
+  describe "get_data!" do
+    it "retrieves more data" do
+      food = subject.new("number" => 44)
+
+      Mat.api.stub(:foodstuff, fixture_data) do
+        food.get_data!.zink.must_equal 0.1
+      end
+    end
+  end
 end
